6a5316bdaa72477a9e370b4524650b64183f2c88
phil-griffith <philgriffith@outlook.com>
2021-05-22 18:24:27 +0000

    Remove secret key and add conditional parameter for debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ubwo0-a!lrz68qahm303fyoba4w5hnuet=dtwh!k0#*(4n7u-r'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pg-boutique-ado.herokuapp.com', 'localhost']
 
